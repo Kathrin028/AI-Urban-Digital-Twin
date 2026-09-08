a317d86fe2bf53439befacf6567d690663bf7b39
Kathrin J <kathrin2006200@gmail.com>
2026-09-08 08:48:44 +0530

    Fix Vercel image case sensitivity

--- frontend/src/components/auth/AuthLayout.jsx
+++ frontend/src/components/auth/AuthLayout.jsx
@@ -1,4 +1,4 @@
-import heroImage from "../../assets/images/hero.png";
+import heroImage from "../../assets/images/Hero.png";
 
 export default function AuthLayout({ children }) {
   return (
--- frontend/src/components/home/Hero.jsx
+++ frontend/src/components/home/Hero.jsx
@@ -1,4 +1,4 @@
-import heroImage from "../../assets/images/hero.png";
+import heroImage from "../../assets/images/Hero.png";
 import Container from "../common/Container";
 import PrimaryButton from "../common/PrimaryButton";
 import SecondaryButton from "../common/SecondaryButton";
